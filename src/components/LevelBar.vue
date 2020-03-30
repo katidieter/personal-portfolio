c6f5ee0078f63defc30c697c68c6f98c8d243b02
Katieli Bianca Dieter <katielidieter@hotmail.com>
2020-03-29 23:23:19 -0300

    Create component for level bar

---
+++ src/components/LevelBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="#level-bar">
+    <div class="secundary-bar">
+      <div
+        class="main-bar"
+        :style="`background-color: ${color}; width: ${levelWidth}`"
+      />
+    </div>
+    <p
+      :style="`width: ${levelWidth};`"
+    >
+      {{ level }}
+    </p>
+  </div>
+</template>
+
+<script>
+const LEVELS = {
+  junior: '100px',
+  intermediate: '200px',
+  advanced: '300px',
+};
+
+export default {
+  name: 'LevalBar',
+  props: {
+    color: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      validator: value => Object.keys(LEVELS).includes(value),
+    },
+  },
+  computed: {
+    levelWidth() {
+      return LEVELS[this.level];
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.main-bar
+  height 10px
+
+.secundary-bar
+  width 300px
+  height 10px
+  background-color #cacaca
+
+p
+  text-align right
+  font-size 12px
+  margin 0px
+</style>
